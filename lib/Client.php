1dcc935c85e5410b794f7eb9f8c85c5b457921f4
Alexey Saif <fsanday@mail.ru>
2025-01-29 18:29:51 +0300

    Change composer, delete version3

--- lib/Client.php
+++ lib/Client.php
@@ -122,7 +122,7 @@ class Client extends BaseClient
     /**
      * Текущая версия библиотеки
      */
-    const SDK_VERSION = '2.9.1';
+    const SDK_VERSION = '2.9.2';
 
     /**
      * Получить список платежей магазина
